2d2054222be452e79b33eb13ed4d95a7ec17c6b0
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-12-28 12:27:52 -0600

    state with interface

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,34 @@ import { createRoot } from 'react-dom/client';
 interface AppProps {
   color?: string;
 }
+interface AppState {
+  counter: number;
+}
 
-class App extends React.Component<AppProps> {
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  increment = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  decrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>Hi there, {this.props.color}</div>;
+    return (
+      <div>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(<App color="red" />);
+root.render(<App />);
