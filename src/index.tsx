ae0c0a52e71502523e49d09a456972f90a74b2a6
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-12-28 13:03:04 -0600

    dummy reducer store

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { thunk } from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-interface AppProps {
-  color?: string;
-}
-interface AppState {
-  counter: number;
-}
-
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = { counter: 0 };
-  }
-
-  increment = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-  decrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.increment}>Increment</button>
-        <button onClick={this.decrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
